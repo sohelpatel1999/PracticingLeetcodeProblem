eedac270bab552e8edf4e8d371bdedfebb00d7fb
sohelpatel1999 <patelsohel1999@gmail.com>
2023-10-31 16:25:29 +0530

    [Sohel] Update: Java Solution.

--- 344. Reverse String/Solution.java
+++ 344. Reverse String/Solution.java
@@ -1,8 +1,8 @@
 public class Solution {
     public void reverseString(String[] s) {
         int first = 0;
-        int last = s.length - 1;
-        while (first < last) {
+        int last = s.length-1;
+        while (first<last) {
             String temp = s[first];
             s[first] = s[last];
             s[last] = temp;
@@ -14,9 +14,9 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String[] s = { "h", "e", "l", "l", "o" };
+        String[] s = {"h","e","l","l","o"};
         solution.reverseString(s);
-
+        
         for (String string : s) {
             System.out.print(string + " ");
         }
