94b0567ebc16ce656b4e02be10e77c1f317c4fdf
sohelpatel1999 <patelsohel1999@gmail.com>
2023-11-02 19:10:11 +0530

    [Sohel] Added: JavaScript Solution.

---
+++ 162. Find Peak Element/Solution.js
@@ -0,0 +1,15 @@
+var findPeakElement = function(nums) {
+    let high = nums[0];
+    let index = 0;
+    for(i = 0 ; i<nums.length;i++){
+        if(nums[i]>high){
+            high = nums[i];
+            index = i;
+        }
+    }
+    return index;
+};
+
+const array = [1,2,3,1]
+const answer = findPeakElement(array);
+console.log(answer);
