89368fdef090c0927f6dfdfdaab024884c95dcb0
sohelpatel1999 <patelsohel1999@gmail.com>
2023-11-06 15:36:54 +0530

    [Sohel] Added: Javascript Solution.

---
+++ 69. Sqrt(x)/Solution.js
@@ -0,0 +1,10 @@
+var mySqrt = function(x) {
+    let count = 0;
+    for (let i = 0; i <= x/i; i++) {
+        count = i; 
+        
+    }
+    return count;
+}
+const answer = mySqrt(49);
+console.log(answer);
