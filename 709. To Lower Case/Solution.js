0beb46a78fdfe4eafbdafe510dff2f1215a5c36c
sohelpatel1999 <patelsohel1999@gmail.com>
2023-11-07 16:05:18 +0530

    [Sohel] Added: JavaScript Solution.

--- 709. To Lower Case/Solution.js
+++ 709. To Lower Case/Solution.js
@@ -1,5 +1,15 @@
-var toLowerCase = function(s) {
+var toLowerCase = function (s) {
+  var ch = s.split("");
 
-    
+  for (let i = 0; i < ch.length; i++) {
+    if (ch[i].charCodeAt(0) >= 65 && ch[i].charCodeAt(0) <= 90) {
+        console.log(ch[i].charCodeAt(0));
+        ch[i] = (String.fromCharCode(ch[i].charCodeAt(0) + 32));
+    }
+  }
+  console.log(ch);
+  return ch.join("");
+};
 
-}
+const ans = toLowerCase("SohelVASDF");
+console.log(ans);
