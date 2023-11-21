d6c137914b95f0a29c4f440656714fc65f8636b7
sohelpatel1999 <patelsohel1999@gmail.com>
2023-11-21 18:46:49 +0530

    [Sohel] Added: Java Solution.

---
+++ 605. Can Place Flowers/Solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                int prev = (i == 0 || flowerbed[i - 1] == 0) ? 0 : 1;
+                int next = (i == flowerbed.length - 1 || flowerbed[i + 1] == 0) ? 0 : 1;
+
+                if (prev == 0 && next == 0) {
+
+                    count++;
+                    flowerbed[i] = 1;
+                    System.out.println(i + " " + count);
+                }
+            }
+            if (count >= n) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] arr = { 1, 0, 0, 0, 1 };
+        int n = 1;
+        boolean answer = solution.canPlaceFlowers(arr, n);
+        System.out.println(answer);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+}
